3fe97116951d42644ea656266d00e502fde349a0
Tumypmyp <gaynoullin@gmail.com>
2022-12-02 16:38:52 +0000

    argparse added

--- manipulator/manipulator.py
+++ manipulator/manipulator.py
@@ -1,13 +1,15 @@
 import sys
 import socket
 import pickle
+import argparse
 
-port = 5000
-if len(sys.argv) > 1:
-    port = sys.argv[1]
+parser = argparse.ArgumentParser(description='Proccess messages got from the controller')
+parser.add_argument('-p', '--port', type=int, required=True, help='port to listen for the tcp connection')
+args = parser.parse_args()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('', port))
+s.bind(('', args.port))
+
 s.listen()
 conn, addr = s.accept()
 
